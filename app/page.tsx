2a2c46bab0c50386e8425c0e1ea29ea4a9c6e183
unknown <batuhanyolcuu@gmail.com>
2023-12-13 13:22:07 +0300

    refactor: add page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import { ModeToggle } from "@/components/toggle-theme";
-
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <ModeToggle />
-    </main>
+    // <div className="bg-red-500 h-screen px-10 md:px-20 mx-auto lg:max-w-[50%]">
+    <div className="bg-red-900 h-screen mx-auto lg:max-w-5xl px-6 py-5">
+      <div>asdasdsd</div>
+    </div>
   );
 }
