66cf2dea6233e1a2916ef785e1ad98c8a5f1f11a
unknown <batuhanyolcuu@gmail.com>
2024-01-08 22:46:38 +0300

    feat: get user hook

---
+++ hooks/useUser.ts
@@ -0,0 +1,14 @@
+import { getUser } from "@/actions/getUser";
+import { Session } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+
+export const useUser = (session: Session) => {
+  const { data: user } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => getUser(session.id),
+  });
+
+  return {
+    user,
+  };
+};
