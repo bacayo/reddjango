235698f1ff826b645bfda0122740c5d82dd707a6
unknown <batuhanyolcuu@gmail.com>
2024-01-08 22:51:05 +0300

    style: container width

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto lg:max-w-5xl sm:px-6 sm:py-5 flex">{children}</div>
+    <div className="mx-auto flex sm:px-6 sm:py-5 lg:max-w-5xl">{children}</div>
   );
 };
 
